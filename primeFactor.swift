2723ea331f8443c34483e7db11f0d74cfa7f7ebb
nooglersoon <63455298+nooglersoon@users.noreply.github.com>
2020-06-07 21:09:02 +0700

    Create primeFactor.swift

---
+++ primeFactor.swift
@@ -0,0 +1,42 @@
+var numbers = 60;
+var array = [Int]()
+var counter = 0;
+var total = 0;
+
+
+for number in 2...numbers {
+    
+    if numbers % number == 0 {
+        
+        counter = 0
+        for digit in 2...number {
+        
+            
+            if number % digit == 0 {
+                counter += 1
+            }else{
+                counter += 0
+            }
+            
+            
+        }
+        if counter == 1  {
+            array.append(number)
+        }
+        
+        
+    }
+    
+    
+}
+
+for loop in 0...array.count-1 {
+    
+    total += array[loop]
+    
+}
+
+print(array)
+print(total)
+
+
